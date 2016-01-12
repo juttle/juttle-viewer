518f4a4cae269525a36c8f6d96813b1340a57549
Matt Nibecker <mnibecker@jut.io>
2016-01-11 17:08:40 -0800

    Add outrigger-client push + watch support for client

--- src/apps/run/main.js
+++ src/apps/run/main.js
@@ -1,7 +1,8 @@
-import url from "fast-url-parser";
+import url from 'fast-url-parser';
 
-import Juttle from "juttle-client-library";
-import { getBundle } from "../../client-lib/utils/api";
+import Juttle from 'juttle-client-library';
+import { getBundle } from '../../client-lib/utils/api';
+import RendezvousSocket from '../../client-lib/utils/rendezvous-socket';
 
 import 'juttle-client-library/dist/juttle-client-library.css';
 import '../sass/main.scss';
@@ -15,19 +16,28 @@ let inputs = new client.Input(document.getElementById("juttle-input-groups"));
 let currentBundle;
 let parsed = url.parse(window.location.href, true);
 
-getBundle(parsed.query.path)
-.then((res) => {
-    currentBundle = res.bundle;
-    return client.describe(currentBundle);
-})
-.then((desc) => {
-    // if we have no inputs go ahead and run
-    if (desc.inputs.length === 0) {
-        view.run(currentBundle);
-    } else {
-        inputs.render(currentBundle);
-    }
-})
+let initBundle = (bundle) => {
+    currentBundle = bundle;
+    client.describe(bundle)
+    .then((desc) => {
+        // if we have no inputs go ahead and run
+        if (desc.inputs.length === 0) {
+            view.run(bundle);
+        } else {
+            inputs.render(bundle);
+        }
+    });
+};
+
+if (parsed.query.path) {
+    client.describe(parsed.query.path)
+    .then(bundle => initBundle(bundle));
+} else if (parsed.query.rendezvous) {
+    let rendezvousUrl = `ws://${outriggerHost}/rendezvous/${parsed.query.rendezvous}`
+    let rendezvous = new RendezvousSocket(rendezvousUrl);
+
+    rendezvous.on('message', msg => initBundle(msg.bundle));
+}
 
 // run btn click
 document.getElementById("btn-run").addEventListener("click", () => {
---
+++ src/client-lib/utils/rendezvous-socket.js
@@ -0,0 +1,43 @@
+import EventEmitter from 'eventemitter3';
+
+class RendezvousSocket extends EventEmitter {
+    constructor(url) {
+        // instantiate super EventEmitter
+        super();
+
+        // setup websocket
+        this._socket = new WebSocket(url);
+
+        this._socket.onopen = (event) => { this.emit("open"); };
+        this._socket.onclose = (event) => { this.emit("close"); };
+        this._socket.onmessage = this._onMessage.bind(this);
+        this._socket.onerror = this._onError.bind(this);
+    }
+
+    _onError(event) {
+        this.emit("error", JSON.parse(event.data));
+    }
+
+    _onMessage(event) {
+        let msg = JSON.parse(event.data);
+
+        // manage pings so the rest of the app doesn't have to
+        if (msg.type === "ping") {
+            this._socket.send(JSON.stringify({
+                type: "pong"
+            }));
+
+            return;
+        }
+
+        this.emit("message", msg);
+    }
+
+    close() {
+        this._socket.close();
+        this._socket = null;
+    }
+
+}
+
+export default RendezvousSocket;
