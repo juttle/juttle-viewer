4d6fd0bebc7132c87bbf2c6f2715831ee25e031d
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2016-01-14 18:42:35 -0800

    add 'show juttle' toggle to view juttle source

--- src/apps/run/main.js
+++ src/apps/run/main.js
@@ -1,8 +1,13 @@
 import url from 'fast-url-parser';
 
+import ReactDOM from 'react-dom';
+import React from 'react';
+
 import Juttle from 'juttle-client-library';
 import * as api from '../../client-lib/utils/api';
 import RendezvousSocket from '../../client-lib/utils/rendezvous-socket';
+
+import JuttleViewer from './components/juttle-viewer';
 
 import 'juttle-client-library/dist/juttle-client-library.css';
 import '../sass/main.scss';
@@ -12,14 +17,17 @@ let outriggerHost = window.location.host;
 let client = new Juttle(outriggerHost);
 let view = new client.View(document.getElementById("juttle-view-layout"));
 let inputs = new client.Input(document.getElementById("juttle-input-groups"));
+let juttleSourceEl = document.getElementById("juttle-source");
 
 let currentBundle;
 let parsed = url.parse(window.location.href, true);
 
 let initBundle = (bundle) => {
+    ReactDOM.render(<JuttleViewer juttleSource={bundle.program} />, juttleSourceEl);
     currentBundle = bundle;
     client.describe(bundle)
     .then((desc) => {
+
         // if we have no inputs go ahead and run
         if (desc.inputs.length === 0) {
             view.run(bundle);
