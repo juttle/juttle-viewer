b9642bc6c7608ca5a3d4c358f67b8f6a3d826c86
Matt Nibecker <mnibecker@jut.io>
2016-02-11 13:07:44 -0800

    Default to host where app is served from
    If not juttleServiceHost is provided.
    
    The doT library was not working as expected. Removed for underscore
    templates instead.

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,19 @@
 let fs = require('fs');
 let path = require('path');
 let express = require('express');
-let dot = require('dot');
+let _ = require('underscore');
 
 let DIST_DIR = path.join(__dirname, '../..', 'dist');
 let INDEX_PATH = path.join(DIST_DIR, 'index.html');
 
 module.exports = (opts) => {
-
-    if (!opts.juttleServiceHost) {
-        let error = new Error('Must provide juttleServiceHost option');
-        error.code = 'NO-JS-HOST';
-        throw error;
-    }
+    opts = opts || {};
 
     let router = express.Router();
     let indexPath = opts.indexPath || INDEX_PATH;
 
     let indexTemplate = fs.readFileSync(indexPath, 'utf8');
-    let formattedIndex = dot.template(indexTemplate)({
+    let formattedIndex = _.template(indexTemplate, { variable: 'info' })({
         JUTTLE_SERVICE_HOST: opts.juttleServiceHost
     });
 
